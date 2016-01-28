1eb7f472df62d07c244d0bcee7cf5191092a82b5
dr-tutrino <melpjackson@gmail.com>
2016-01-28 13:18:04 +0000

    Update creative.js

--- js/creative.js
+++ js/creative.js
@@ -5,6 +5,25 @@
  */
 
 (function($) {
+
+
+var message = "";
+
+$("#sendMessage").on("click", function() {
+    message = $("#contactform").serialize();
+    $.ajax({
+        url: "//formspree.io/dynamicrealities@gmail.com", 
+        method: "POST",
+        data: {message: message},
+        dataType: "json"
+    });
+    alert('Thanks for the email, we\'ll be in touch promptly.');
+    return false;
+});
+
+
+
+
     "use strict"; // Start of use strict
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
@@ -46,3 +65,5 @@
     new WOW().init();
 
 })(jQuery); // End of use strict
+
+
