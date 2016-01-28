3cfcc2013eb5510340da35a6589d24db9f671c32
dr-tutrino <melpjackson@gmail.com>
2016-01-28 13:22:43 +0000

    Update creative.js

--- js/creative.js
+++ js/creative.js
@@ -7,23 +7,6 @@
 (function($) {
 
 
-var message = "";
-
-$("#sendMessage").on("click", function() {
-    message = $("#contactform").serialize();
-    $.ajax({
-        url: "//formspree.io/dynamicrealities@gmail.com", 
-        method: "POST",
-        data: {message: message},
-        dataType: "json"
-    });
-    alert('Thanks for the email, we\'ll be in touch promptly.');
-    return false;
-});
-
-
-
-
     "use strict"; // Start of use strict
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
