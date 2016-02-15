50609cfa41963acf40088166ce3ef45556ad18c8
dr-tutrino <melpjackson@gmail.com>
2016-02-15 15:37:45 +0000

    Update creative.js

--- js/creative.js
+++ js/creative.js
@@ -47,12 +47,13 @@
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
 
+
+var $pupil_form = $('#pupil_form');
 var $tutor_form = $('#tutor_form');
 var modal = document.getElementById('myModal');
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-var $pupil_form = $('#pupil_form');
 
 $pupil_form.submit(function(e) {
     e.preventDefault();
@@ -83,6 +84,35 @@ $pupil_form.submit(function(e) {
     }   
 });
 
+
+$tutor_form.submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        url: '//formspree.io/melpjackson@gmail.com',
+        method: 'POST',
+        data: $(this).serialize(),
+        dataType: 'json',
+        beforeSend: function() {
+        },
+        success: function(data) {
+            $('#myModal').modal();
+        },
+        error: function(err) {
+            $tutor_form.append('<div class="alert alert--error">Ops, there was an error.</div>');
+        }
+    });
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+         modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }   
+});
 
 function show_pupil_form() {
     $("#pupil_form").slideDown();
