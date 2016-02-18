e03df0e1605dfcfaa3a9d9f104344354600cd2f8
dr-tutrino <melpjackson@gmail.com>
2016-02-18 15:20:31 +0000

    Update creative.js

--- js/creative.js
+++ js/creative.js
@@ -149,7 +149,7 @@ $('.js-tutor-btn').click(function () {
 
 $('.thumbnail').click(function(){
   	$('.modal-body').empty();
-  	var title = $(this).nextSibling;
+  var title = $(this).parent('a').attr("title");
   	$('.modal-title').html(title);
   	$($(this).parents('div').html()).appendTo('.modal-body');
   	$('#lightbox-modal').modal({show:true});
