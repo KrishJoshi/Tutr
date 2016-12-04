bbcfe18eb9ca95ac4b780c0bb27893c5aa498d03
dr-tutrino <melpjackson@gmail.com>
2016-12-04 13:04:57 +0000

    Update creative.js

--- js/creative.js
+++ js/creative.js
@@ -167,15 +167,13 @@ $('.thumbnail').click(function(){
  
    $('.modal-body').empty();
  
+  	$($(this).parents('div').html()).appendTo('.modal-body');//try this one 
  
- 
-    var newTr = $("#julTable").clone().show();
-  //  newTr.style.visibility ="visible";
+   var newTr = $("#julTable").clone().show();
+   //  newTr.style.visibility ="visible";
     newTr.css({"float": "right", "font-size": "40%"});
     newTr.appendTo($(".modal-body"));
  
-  	$('.modal-text').empty();
-  	$($(this).parents('div').html()).appendTo('.modal-body');//try this one 
   	$('.modal-text').append($($(this).parents('div').html()).text());
   	$('#myModal').modal({show:true});
 });
