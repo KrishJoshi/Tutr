7ae0d65a3bdcef043c64ae64a54f189c56fcc7dd
dr-tutrino <melpjackson@gmail.com>
2016-12-04 17:48:38 +0000

    Update creative.js

--- js/creative.js
+++ js/creative.js
@@ -172,9 +172,10 @@ $('.thumbnail').click(function(){
  //  var newTr = $("#julTable").clone().show();
     var newTr =  $(this).siblings("table").clone().show();
     newTr.css({"width": "200px", "height":"300px", "position": "absolute", "top":"10px","right":"10px" });
+    newTr.appendTo($(".modal-body"));
  
     var desc =  $(this).siblings("p").clone().show();
-    newTr.appendTo($(".modal-body"));
+    desc.appendTo($(".modal-text"));
  
   	$('#myModal').modal({show:true});
 });
