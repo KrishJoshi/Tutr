3d249ef365e3a1e26c5d50e076e04bc3a025f7f3
dr-tutrino <melpjackson@gmail.com>
2016-12-10 20:28:35 +0000

    Update creative.js

--- js/creative.js
+++ js/creative.js
@@ -169,10 +169,12 @@ $('.thumbnail').click(function(){
  
   	$($(this).parents('div').html()).appendTo('.modal-body');//try this one 
  
- //  var newTr = $("#julTable").clone().show();
-    var newTr =  $(this).siblings("table").clone().show();
+ if (document.documentElement.clientWidth > 650) {
+	    var newTr =  $(this).siblings("table").clone().show();
     newTr.css({"width": "200px", "height":"300px", "position": "absolute", "top":"-10px","right":"10px" });
     newTr.appendTo($(".modal-body"));
+ }  
+ 
 
     var newTr2 =  $(this).siblings("p").clone().show();
    newTr2.appendTo($(".modal-text"));
